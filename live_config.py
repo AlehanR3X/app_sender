728fff585abef2105ae28ab00bb8438198d0656a
AlehanR3X <alehandromatias@outlook.com>
2025-04-23 21:44:38 -0500

    live_config.py

---
+++ live_config.py
@@ -0,0 +1,8 @@
+GROUPS = [
+    {"name": "StripeAuthScrapper", "chat_id": -1001234567890},
+    {"name": "Lions_scrapperfree", "chat_id": -1009876543210},
+    {"name": "freedropcards", "chat_id": -1001122334455},
+    {"name": "Otro", "chat_id": None},
+    {"name": "NuevoGrupo", "chat_id": -1002304417121},
+    {"name": "GrupoExtra", "chat_id": -1001849372296}
+]
